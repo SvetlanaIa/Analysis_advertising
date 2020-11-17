42b01a5777957cc5391c12d40ca1cd005e6c52bf
Яковлева Светлана <svetlana1905.iakovleva@yandex.ru>
2020-11-17 12:44:16 +0300

    add fixture

--- test.py
+++ test.py
@@ -24,11 +24,6 @@ def transform_info (adv,data):
 class TestClass:
     
     def test_fields(self,transform_info,data):
-        # file_name = 'amo_json_2020_40.json'
-        # adv = Analysis(CONFIG)
-        # data = adv.extract(file_name)
-        # transform_info = adv.transform(data)
-
         assert transform_info[0]['id'] == data[0]['id'], 'Проверить id'
         assert transform_info[0]['created_at_year'] == '2020', 'Неверный расчет created_at_year'
         assert transform_info[0]['created_a_month'] == '10', 'Неверный расчет created_a_month'
@@ -46,10 +41,6 @@ class TestClass:
 
 
     def test_custom_fields(self,transform_info,data,adv):
-        # file_name = 'amo_json_2020_40.json'
-        # adv = Analysis(CONFIG)
-        # data = adv.extract(file_name)
-        # transform_info = adv.transform(data)
         assert transform_info[3]['amo_city'] == 'Москва', 'Неверный расчет amo_city'
         assert transform_info[3]['drupal_utm'] == 'source=google, medium=context, campaign=blue, keyword=1351759424-77001425413-378733477874--none--doneto.ru--, content=cntx', 'Неверный расчет drupal_utm'
         assert transform_info[3]['tilda_utm_source'] == None, 'Неверный расчет tilda_utm_source'
